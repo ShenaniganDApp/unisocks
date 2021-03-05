774dcdbfa96bacdd832b60966a6f845507467ccb
YoungKidWarrior <ginelliunit@gmail.com>
2021-03-05 01:49:45 -0800

    ugh

--- src/pages/App.js
+++ src/pages/App.js
@@ -35,11 +35,11 @@ export default function App() {
             <AppProvider>
               <BrowserRouter>
                 <Switch>
-                  <Route exact strict path="/" render={() => <Main />} />
+                  {/* <Route exact strict path="/" render={() => <Main />} />
                   <Route exact strict path="/status" render={() => <Main status />} />
                   <Route exact strict path="/stats" render={() => <Main stats />} />
                   <Route exact strict path="/staking" render={() => <Main staking />} />
-                  <Route exact strict path="/migrate" render={() => <Main migration />} />
+                  <Route exact strict path="/migrate" render={() => <Main migration />} /> */}
                   <Redirect to="/" />
                 </Switch>
               </BrowserRouter>
